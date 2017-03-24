4fc97ce4e16ec0427a0cce7a8cf17e140ec1e13b
ahees <ahees@ex.ua>
2017-03-24 14:03:10 +0200

    up

--- demo/easyway/app/js/common.js
+++ demo/easyway/app/js/common.js
@@ -1,5 +1,11 @@
 $(function() {
 
 	// Custom JS
-
+	function heightDetect() {
+		$(".auto_height").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+});
 });
