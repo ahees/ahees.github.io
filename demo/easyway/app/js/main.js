e97b9a205d23cc1b73c1d60806dfc48b97e47ac9
ahees <ahees@ex.ua>
2017-03-25 21:02:42 +0200

    1

--- demo/easyway/app/js/main.js
+++ demo/easyway/app/js/main.js
@@ -21,7 +21,37 @@ $(function() {
 		imageSrc: './img/bg2_min.jpg',
 		speed: 0.1
 	});
-
+	
+	$(".owl-carousel").owlCarousel({
+	    loop:true,	    
+	    responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1
+	            
+	        },
+	        600:{
+	            items:1,
+	            nav:false
+	        },
+	        1000:{
+	            items:1
+	            
+	        }
+	    }
+	});
+	var owl = $('.owl-carousel');
+	owl.owlCarousel();
+	// Go to the next item
+	$('.owl_right').click(function() {
+	    owl.trigger('next.owl.carousel');
+	})
+	// Go to the previous item
+	$('.owl_left').click(function() {
+	    // With optional speed parameter
+	    // Parameters has to be in square bracket '[]'
+	    owl.trigger('prev.owl.carousel', [300]);
+	})
 
 
 	//Chrome Smooth Scroll
