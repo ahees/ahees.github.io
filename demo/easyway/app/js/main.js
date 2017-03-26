82a307abe147ed6b9c6f7b1afaeb98a988425b17
ahees <ahees@ex.ua>
2017-03-26 12:32:40 +0300

    up

--- demo/easyway/app/js/main.js
+++ demo/easyway/app/js/main.js
@@ -77,6 +77,21 @@ $(function() {
 
 	$(".top_menu .menu a").mPageScroll2id();
 
+	$('.feedback').magnificPopup({
+        type: 'inline'		
+    });
+
+	$('.gallery_items').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Загрузка изображения #%curr%...',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+		}
+	});
+
 
 
 
