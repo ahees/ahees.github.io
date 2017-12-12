9398591bdfcd577c42f17aa1ae253c417ff4c541
ahees <ahees@ex.ua>
2017-12-12 14:40:16 +0200

    up
    up

---
+++ fd/js/common.js
@@ -0,0 +1,14 @@
+$(function() {
+
+	// Custom JS
+
+});
+
+var app = new Vue({
+	el: '#app',
+	data: {
+	  message: 'Hello Vue!',
+	  // test: 'https://scontent-frt3-1.cdninstagram.com/t51.2885-15/e35/23416356_131880697520615_3295070416678158336_n.jpg'
+	  test: ''
+	}
+  })
