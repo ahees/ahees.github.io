b2259f6b81fc1a1d33181236c89eb9bee56cfada
ahees <ahees@ex.ua>
2018-03-26 01:10:08 +0300

    1

--- demo/pvd/js/common.js
+++ demo/pvd/js/common.js
@@ -7,4 +7,37 @@ $(function() {
 		$(".hamburger").toggleClass("is-active");
 	});
 
+	var owl = $('.carousel-serv');
+	owl.owlCarousel({
+		loop:true,
+		dots:false,
+		autoHeight:true,
+		autoplay:true,
+		margin:10,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+				nav:false
+			},
+			600:{
+				items:1,
+				nav:false
+			},
+			1000:{
+				items:1,
+				nav:false				
+			}
+		}
+	});
+	$('#nextcarousel').click(function() {
+		owl.trigger('next.owl.carousel');
+	})
+	// Go to the previous item
+	$('#prevcarousel').click(function() {
+		// With optional speed parameter
+		// Parameters has to be in square bracket '[]'
+		owl.trigger('prev.owl.carousel', [300]);
+	})
+
 });
