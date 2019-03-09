e6dcaa70da73bf1c49c1f796b38be8b6c3f26f7a
ahees <ahees@ex.ua>
2019-03-09 18:22:03 +0200

    1

--- demo/medlaw/js/common.js
+++ demo/medlaw/js/common.js
@@ -1,5 +1,35 @@
 $(function() {
 
+    /* form */
+    $('#yesdata').change(function () {
+        if (this.checked) {
+            $('#my-submit').prop('disabled', false);
+        } else {
+            $('#my-submit').prop('disabled', true);
+        }
+    });
+
+    $('#cost').change(function () {
+        if ($('#cost').val() == 'Інше') {
+            $('#cost-other').show("slow");
+        } else $('#cost-other').hide("slow");
+        console.log($('#cost').val());
+    });
+
+    $('.no-check').on('click', function () {
+        $('.zmi-block, .other-block').hide("slow")
+    })
+    $('#check_zmi').on('click', function () {
+        $('.zmi-block').show("slow");
+        $('.other-block').hide("slow");
+    });
+    $('#check_other').on('click', function () {
+        $('.other-block').show("slow");
+        $('.zmi-block').hide("slow");
+    });
+
+/*end form*/
+    
     
 
 });
